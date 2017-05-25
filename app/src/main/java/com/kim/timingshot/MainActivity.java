d525177378f046241bc0b4eb7dbf12e938ec3623
MunSeongKim <chyin370@naver.com>
2017-05-26 07:32:53 +0900

    Made to Main Activity
    메인화면 작업 완료

--- app/src/main/java/com/kim/timingshot/MainActivity.java
+++ app/src/main/java/com/kim/timingshot/MainActivity.java
@@ -1,13 +1,21 @@
 package com.kim.timingshot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tsengvn.typekit.TypekitContextWrapper;
 
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
+    }
+
 }
---
+++ app/src/main/java/com/kim/timingshot/SetFontApp.java
@@ -0,0 +1,21 @@
+package com.kim.timingshot;
+
+import android.app.Application;
+
+import com.tsengvn.typekit.Typekit;
+
+/**
+ * Created by Administrator on 2017-05-26.
+ */
+
+public class SetFontApp extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        Typekit.getInstance()
+                .addItalic(Typekit.createFromAsset(this, "fonts/speed.ttf"))
+                .addBold(Typekit.createFromAsset(this, "fonts/BusanStation.ttf"))
+                .addNormal(Typekit.createFromAsset(this, "fonts/ENORBITB.TTF"));
+    }
+}
